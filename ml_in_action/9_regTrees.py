b2726aaeba0df9382ce55f1814e5401671f6dd27
rayanral <rayanral@gmail.com>
2016-12-15 14:29:10 +0200

    ML in Action - CART tree implementation finished

--- ml_in_action/9_regTrees.py
+++ ml_in_action/9_regTrees.py
@@ -24,6 +24,46 @@ def binSplitDataSet(dataSet, feature, value):
     mat1 = dataSet[nonzero(dataSet[:,feature] <= value)[0], :][0]
     return mat0, mat1
 
+
+def regLeaf(dataset):
+    return mean(dataset[:, -1])
+
+
+# calculates total squared error
+def regErr(dataset):
+    return var(dataset[:, -1]) * shape(dataset)[0]
+
+
+def chooseBestSplit(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
+    tolS = ops[0]  # tolerance on error
+    tolN = ops[1]  # tolerance on minimum data instances
+    # Exit if all values are equal
+    if len(set(dataSet[:,-1].T.toList()[0])) == 1:
+        return None, leafType(dataSet)
+    m, n = shape(dataSet)
+    S = errType(dataSet)
+    bestS = inf
+    best_feature = 0
+    best_value = 0
+    for featIndex in range(n-1):
+        for splitVal in set(dataSet[:,featIndex]):
+            mat0, mat1 = binSplitDataSet(dataSet, featIndex, splitVal)
+            if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN): continue
+            newS = errType(mat0) + errType(mat1)  # calculating error for possible split
+            if newS < bestS:
+                best_feature = featIndex
+                bestS = newS
+                best_value = splitVal
+    if (S - bestS) < tolS:
+        return None, leafType(dataSet)
+    mat0, mat1 = binSplitDataSet(dataSet, best_feature, best_value)
+    if (shape(mat0)[0] < tolN) or (shape(mat1)[0] < tolN):
+        return None, leafType(dataSet)
+    return best_feature, best_value
+
+
+
+
 def createTree(dataSet, leafType=regLeaf, errType=regErr, ops=(1,4)):
     feat, val = chooseBestSplit(dataSet, leafType, errType, ops)
     if feat == None: return val
